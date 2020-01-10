242d1f6382c2337e6e12e6fd4009f9dab90ec62d
bill fu <bill.fu@hotmail.com>
2020-01-10 16:07:52 +0800

    www

--- Tacotron-2/bb_py2ipa_v3.sh
+++ Tacotron-2/bb_py2ipa_v3.sh
@@ -1,4 +1,4 @@
 python3.6 py2ipa_v3.py \
---in_pinyin_file=pinyin_1w_fixed_v2.txt \
---out_ipa_file=ipa_1w_v3.txt
+--in_pinyin_file=../bb_text/py_ehy_fixed.txt \
+--out_ipa_file=../bb_text/ipa_v3.txt
 
