aeac2d22fea02b06e6c4d2c1b6a631b05c40aeed
BillFu <Bill.Fu@hotmail.com>
2020-02-05 18:12:38 +0800

    www

---
+++ Tacotron-2/bb_t2_preprocess_win.sh
@@ -0,0 +1,1 @@
+python3 biaobei_preprocess.py --wave_dir=/Volumes/thchs30/BZNSYP/Wave --pinyin_anno=pinyin_1w_fixed_v2.txt
---
+++ Tacotron-2/change_sr_sox_16k_wsl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INPUT_WAV_DIR=/mnt/e/BZNSYP/Wave
+OUTPUT_WAV_DIR=/mnt/e/BZNSYP/Wave_16k
+NEW_SR=16000
+
+mkdir -p ${OUTPUT_WAV_DIR}
+
+echo "new sampled *.wav files will be saved at ./${OUTPUT_WAV_DIR}"
+for in_wav_file in ${INPUT_WAV_DIR}/*.wav; do
+    # echo $in_wav_file
+    basename="${in_wav_file##*/}"
+    new_name=${OUTPUT_WAV_DIR}/${basename}
+    echo ${new_name}
+    # sox  ${in_wav_file} -r ${NEW_SR} ${new_name}
+done
