a9d5686c3649f5d7d69e2ff18ebfa742053d5130
Bill Fu <bill.fu@hotmail.com>
2020-04-28 19:42:47 +0800

    www

--- lpcnet_test/all_in_one.sh
+++ lpcnet_test/all_in_one.sh
@@ -7,6 +7,6 @@ chmod a+x lpcnet_infer
 
 ./lpcnet_infer feature_1001.f32 synth_pcm1.f16
 
-ffmpeg -f s16le -ar 16k -ac 1 -i synth_pcm1.f16 -ar 16k synth_16k_1.wav
-ffmpeg -f s16le -ar 16k -ac 1 -i synth_pcm1.f16 -ar 8k  synth_8k_1.wav
+# ffmpeg -f s16le -ar 16k -ac 1 -i synth_pcm1.f16 -ar 16k synth_16k_1.wav
+# ffmpeg -f s16le -ar 16k -ac 1 -i synth_pcm1.f16 -ar 8k  synth_8k_1.wav
 
