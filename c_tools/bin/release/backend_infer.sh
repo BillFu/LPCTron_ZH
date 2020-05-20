44591b3dcfb84bc38e5b2bb5a7f26ae10ccfaa5e
bill fu <bill.fu@hotmail.com>
2020-05-20 17:48:32 +0800

    www

--- c_tools/bin/release/backend_infer.sh
+++ c_tools/bin/release/backend_infer.sh
@@ -1,13 +1,6 @@
 
-# chmod a+x dump_data_lpcnet
-# ./dump_data_lpcnet -test audio-000001.s16 features1.f32
+mkdir -p output 
 
-chmod a+x lpcnet_infer
+chmod a+x backend_infer
 
-chmod a+x taco_infer_c_api
-
-./taco_infer_c_api 1008_ipa.txt tacotron.pb 1008_feature.dat
-
-./lpcnet_infer 1008_feature.dat 1008_pcm.f16
-
-ffmpeg -f s16le -ar 16k -ac 1 -i 1008_pcm.f16 -ar 16k 1008_16k_5.wav
+./backend_infer 1008 1008_ipa.txt tacotron.pb 16k output 
--- c_tools/lpctron_infer/backend_infer.cpp
+++ c_tools/lpctron_infer/backend_infer.cpp
@@ -5,14 +5,18 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include<sstream>
+#include <sstream>
 #include <string>
 
 #include "TFUtils.hpp"
 #include <tensorflow/c/c_api.h>
+#include "lpcnet.h"
+#include "arch.h"
+#include "freq.h"
 
 using namespace std;
 
+// merge Tacotron-2 and LPCNet into this module.
 
 // if ok, return true; otherwise return false.
 bool load_ipa_id_seq(const string& ipa_file_name, vector<int>& ipa_id_list)
@@ -39,59 +43,34 @@ bool load_ipa_id_seq(const string& ipa_file_name, vector<int>& ipa_id_list)
     return true;
 }
 
-int main(int argc, char* argv[])
+// return true for OK;
+// otherwise false for error happened, and error_msg contains the detail.
+bool infer(TFUtils& TFU, const string& ipa_seq_file_name,
+        LPCNetState* lpcnet, const string& out_sr,
+        const string& out_pcm_file_name, const string& out_wave_file_name,
+        string& error_msg)
 {
-    if (argc != 4)
-    {
-        std::cerr << std::endl <<
-            "Usage: ./taco_infer_cpp <ipa_id_seq file> <tacotron model file> <out_feature_file.dat>" << std::endl;
-        return 1;
-    }
-
-    std::string ipa_seq_file_name = argv[1];
-    std::string taco_model_file_name = argv[2];
-    std::string out_feature_file_name = argv[3];
-
-    cout << "IPA ID Seq File: " << ipa_seq_file_name << endl;
-    cout << "Tacotron Model File: " << taco_model_file_name << endl;
-
-    // TFUtils init
-    TFUtils TFU;
-    TFUtils::STATUS status = TFU.LoadModel(taco_model_file_name);
-    if (status != TFUtils::SUCCESS)
-    {
-        std::cerr << "Can't load tacotron graph from file: " << taco_model_file_name << endl;
-        return 1;
-    }
-    else
-    {
-        cout << "Tacotron Model has been loaded Successfully!" << endl;
-    }
-
     TF_Graph* graph = TFU.getGraph();
 
     vector<int> ipa_id_list;
     bool is_OK = load_ipa_id_seq(ipa_seq_file_name, ipa_id_list);
     if(!is_OK)
     {
-        std::cerr << "Can't load ipa id seq from file: " << ipa_seq_file_name << endl;
-        return 1;
+        error_msg = "Can't load ipa id seq from file: "  + ipa_seq_file_name;
+        return false;
     }
     else
     {
-        std::cerr << "ipa id seq loaded successfully." << endl;
+        std::cout << "ipa id seq loaded successfully." << endl;
     }
-
-    //auto status0 = TF_NewStatus();
-    //SCOPE_EXIT{ TF_DeleteStatus(status0); }; // Auto-delete on scope exit.
 
     TF_Output inputs_op = TFU.GetOperationByName("inputs", 0);
     TF_Output input_lengths_op = TFU.GetOperationByName("input_lengths", 0);
 
     if((inputs_op.oper == nullptr) || (input_lengths_op.oper == nullptr))
     {
-        cout << "inputs OP or input_lengths OP NOT found!" << endl;
-        return 1;
+        error_msg = "inputs OP or input_lengths OP NOT found!";
+        return false;
     }
 
     // Input Tensor Create
@@ -106,8 +85,8 @@ int main(int argc, char* argv[])
             TF_INT32, input_lengths_dims, input_lengths_vals);
     if((inputs_tensor == nullptr) || (input_lengths_tensor == nullptr))
     {
-        cout << "inputs_tensor or input_lengths_tensor are NULL!" << endl;
-        return 1;
+        error_msg = "inputs_tensor or input_lengths_tensor are NULL!";
+        return false;
     }
 
     const std::vector<TF_Output> all_input_ops = {inputs_op, input_lengths_op};
@@ -117,58 +96,129 @@ int main(int argc, char* argv[])
     TF_Output out_op = TFU.GetOperationByName("model/inference/add", 0);
     if(out_op.oper == nullptr)
     {
-        cout << "mel_outputs op NOT found!" << endl;
-        return 1;
+        error_msg = "mel_outputs op NOT found!";
+        return false;
     }
 
     const std::vector<TF_Output> output_ops = {out_op};
     std::vector<TF_Tensor*> output_tensors = {nullptr};
 
-    status = TFU.RunSession(all_input_ops, all_input_tensors,
+    TFUtils::STATUS status = TFU.RunSession(all_input_ops, all_input_tensors,
                             output_ops, output_tensors);
 
     TFUtils::PrinStatus(status);
 
-    if (status == TFUtils::SUCCESS)
+    if (status != TFUtils::SUCCESS)
     {
-        //https://github.com/tensorflow/tensorflow/blob/master/tensorflow/c/c_api_test.cc
-        cout << "run session successfully done!"<< endl;
-        TF_Tensor* mels_out_tensor = output_tensors[0];
+        TFUtils::DeleteTensors(all_input_tensors);
+        TFUtils::DeleteTensors(output_tensors);
 
-        int dims = TF_NumDims(mels_out_tensor);
-        cout << "the dims of mels_out_tensor: "<< dims << endl;
-
-        int dim0 = TF_Dim(mels_out_tensor, 0);
-        int dim1 = TF_Dim(mels_out_tensor, 1);
-        int dim2 = TF_Dim(mels_out_tensor, 2);
-
-        cout << "the dim 0 of mels_out_tensor: "<< dim0 << endl;
-        cout << "the dim 1 of mels_out_tensor: "<< dim1 << endl;
-        cout << "the dim 2 of mels_out_tensor: "<< dim2 << endl;
-
-        cout << "tensor type: " << TF_TensorType(mels_out_tensor) << endl;
-        int num_bytes = TF_TensorByteSize(mels_out_tensor);
-        cout << "Bytes: " << num_bytes << endl;
-
-        float* mels_data = static_cast<float*>(TF_TensorData(mels_out_tensor));
-
-        ofstream out(out_feature_file_name, ios::out | ios::binary);
-        if(!out) {
-            cout << "Cannot open out feature data file.";
-            return 1;
-        }
-
-        out.write((char*)mels_data, num_bytes);
-        out.close();
+        error_msg = "Error run session in Tacotron reasoning phase.";
+        return false;
     }
-    else
+
+    //https://github.com/tensorflow/tensorflow/blob/master/tensorflow/c/c_api_test.cc
+    cout << "run session successfully done!"<< endl;
+    TF_Tensor* mels_out_tensor = output_tensors[0];
+
+    int num_frames = TF_Dim(mels_out_tensor, 1);
+    cout << "num_frames: " << num_frames << endl;
+
+    int num_bytes = TF_TensorByteSize(mels_out_tensor);
+    cout << "Bytes: " << num_bytes << endl;
+
+    float* mels_data = static_cast<float*>(TF_TensorData(mels_out_tensor));
+
+    ofstream ofs_pcm(out_pcm_file_name, ios::binary);
+
+    float* raw_features = mels_data;
+    int bytes_frame = sizeof(short) * FRAME_SIZE;
+    for (int i = 0; i < num_frames; i++)
     {
-        cout << "Error run session" << endl;
-        return 1;
+        float in_features[NB_FEATURES];
+        short out_pcm[FRAME_SIZE];
+
+        RNN_COPY(in_features, raw_features, NB_BANDS);
+        RNN_CLEAR(in_features+18, 18);
+        RNN_COPY(in_features+36, raw_features+NB_BANDS, 2);
+
+        lpcnet_synthesize(lpcnet, out_pcm, in_features, FRAME_SIZE);
+        ofs_pcm.write(reinterpret_cast<char *>(out_pcm), bytes_frame);
+
+        raw_features = raw_features + (NB_BANDS+2);  // move to the next frame
     }
+    ofs_pcm.close();
 
     TFUtils::DeleteTensors(all_input_tensors);
     TFUtils::DeleteTensors(output_tensors);
+
+    string ffmpeg_cmd;
+    if(out_sr == "16k")
+        ffmpeg_cmd = "ffmpeg -f s16le -ar 16k -ac 1 -i " +
+            out_pcm_file_name + " -ar 16k " + out_wave_file_name;
+    else  // 8k for the output wav file
+        ffmpeg_cmd = "ffmpeg -f s16le -ar 16k -ac 1 -i " +
+            out_pcm_file_name + " -ar 8k " + out_wave_file_name;
+
+    system(ffmpeg_cmd.c_str());
+
+    return true;
+}
+
+
+int main(int argc, char* argv[])
+{
+    // <out_sr> must be "8k" or "16k"
+    if (argc != 6)
+    {
+        std::cerr << std::endl <<
+        "Usage: ./backend_infer <sentence_id> <ipa_id_seq file> <tacotron model file> " <<
+        "<out_sr> <out_folder>" << std::endl;
+        return 1;
+    }
+
+    std::string sentence_id = argv[1];
+    std::string ipa_seq_file_name = argv[2];
+    std::string taco_model_file_name = argv[3];
+    std::string out_sr = argv[4];
+    string out_folder = argv[5];
+    string out_pcm_file_name = out_folder + "/" + sentence_id + ".pcm";
+    string out_wave_file_name =  out_folder + "/" + sentence_id + ".wav";;
+
+    cout << "sentence_id: " << sentence_id << endl;
+    cout << "IPA ID Seq File: " << ipa_seq_file_name << endl;
+    cout << "Tacotron Model File: " << taco_model_file_name << endl;
+    cout << "out sr: " << out_sr << endl;
+    cout << "out_folder: " << out_folder << endl;
+    cout << "out pcm file: " << out_pcm_file_name << endl;
+    cout << "out wave file: " << out_wave_file_name << endl;
+
+    // TFUtils init
+    TFUtils TFU;
+    TFUtils::STATUS status = TFU.LoadModel(taco_model_file_name);
+    if (status != TFUtils::SUCCESS)
+    {
+        std::cerr << "Can't load tacotron model from file: " << taco_model_file_name << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Tacotron Model has been loaded Successfully!" << endl;
+    }
+
+    LPCNetState* lpcnet;
+    lpcnet = lpcnet_create();
+
+    std::string  error_msg;
+
+    bool isOK = infer(TFU, ipa_seq_file_name, lpcnet, out_sr,
+                      out_pcm_file_name, out_wave_file_name, error_msg);
+    if (!isOK)
+    {
+        cout << "Error: " << error_msg << endl;
+    }
+
+    lpcnet_destroy(lpcnet);
 
     return 0;
 }
