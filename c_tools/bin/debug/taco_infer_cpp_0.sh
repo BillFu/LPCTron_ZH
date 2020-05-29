b5dc0e533ed23b2d623227b1aa917fddea96cf97
Bill Fu <bill.fu@hotmail.com>
2020-05-29 22:00:57 +0800

    www

---
+++ c_tools/bin/debug/taco_infer_cpp_0.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+chmod a+x taco_infer_cpp_0
+
+./taco_infer_cpp_0 1008_ipa.txt tacotron.pb 
---
+++ c_tools/bin/release/taco_infer_cpp.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+chmod a+x taco_infer_cpp
+
+./taco_infer_cpp 1008_ipa.txt tacotron.pb 
