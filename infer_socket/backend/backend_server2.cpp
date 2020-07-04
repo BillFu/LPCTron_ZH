a354b7dbeef22d61ba0632f12b46ef1cfc52b842
Bill Fu <bill.fu@hotmail.com>
2020-07-04 10:28:00 +0800

    www

--- infer_socket/backend/backend_server2.cpp
+++ infer_socket/backend/backend_server2.cpp
@@ -1,8 +1,57 @@
 #include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
 #include <arpa/inet.h>
 #include <string>
 #include <iostream>
 
+struct info {
+    const char *name;
+    size_t total_drained;
+};
+
+void read_callback(struct bufferevent *bev, void *ctx)
+{
+    struct evbuffer* input = bufferevent_get_input(bev);
+    size_t data_len = evbuffer_get_length(input);
+    if (data_len)
+    {
+        char in_msg[1024];
+
+        int result = evbuffer_remove(input, in_msg, data_len);
+        if(result == -1) //failure
+        {
+            printf("received msg: %s\n", in_msg);
+            return;
+        }
+
+        in_msg[data_len] = '\n';
+        printf("length of msg %d bytes \n", (int)data_len);
+        printf("received msg: %s\n", in_msg);
+    }
+}
+
+void event_callback(struct bufferevent *bev, short events, void *ctx)
+{
+    struct evbuffer *input = bufferevent_get_input(bev);
+    int finished = 0;
+
+    if (events & BEV_EVENT_EOF) {
+        size_t len = evbuffer_get_length(input);
+        printf("socket closed.\n");
+        finished = 1;
+    }
+    if (events & BEV_EVENT_ERROR) {
+        printf("socket error: %s\n",
+               evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR()));
+        finished = 1;
+    }
+    if (finished) {
+        //free(ctx);
+        bufferevent_free(bev);
+    }
+}
 
 void on_conn_accepted(struct evconnlistener *listener,
                     evutil_socket_t fd,
@@ -13,6 +62,17 @@ void on_conn_accepted(struct evconnlistener *listener,
     auto *sin = reinterpret_cast<sockaddr_in *>(address);
     inet_ntop(AF_INET, &sin->sin_addr, addr, INET_ADDRSTRLEN);
     std::cout << "Accept TCP connection from: " << addr << std::endl;
+
+    /* We got a new connection! Set up a bufferevent for it. */
+    struct event_base* base = evconnlistener_get_base(listener);
+    struct bufferevent* buf_evt = bufferevent_socket_new(
+            base, fd, BEV_OPT_CLOSE_ON_FREE);
+    //This function returns a bufferevent on success, and NULL on failure.
+
+    bufferevent_setcb(buf_evt, read_callback, NULL, event_callback, NULL);
+
+    bufferevent_enable(buf_evt, EV_READ|EV_WRITE);
+    //By default, a newly created bufferevent has writing enabled, but not reading.
 }
 
 void accept_error_cb(struct evconnlistener *listener, void *arg)
